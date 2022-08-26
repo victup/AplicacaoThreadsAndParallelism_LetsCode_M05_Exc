b44160f9876900f2700ba7f6bf70e3a343d52ab2
victup <victor.gsantos@hotmail.com>
2022-08-26 15:48:51 -0300

    Add project files.

---
+++ Ex01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Dictionary<string, double> contaBancariaClientes = new Dictionary<string, double>()
+   {
+       {"Murilo Fillo", 2300.38},
+       {"João Pereira", 4850.50},
+       {"Marcos Peixada", 865.45},
+       {"Irineu Lottus", 100},
+       {"Maria Lina", 26450.89}
+   };
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        ParallelOptions op = new();
+        op.MaxDegreeOfParallelism = 5;
+
+
+        Parallel.For(0, 100, op, i =>
+        {
+            GetBalance(PegarNomeAleatorio(contaBancariaClientes), contaBancariaClientes);
+        });
+
+        stopwatch.Stop();
+
+        ImprimirTempoEmSegundos(stopwatch);
+
+    }
+    static void GetBalance(string name_customer, Dictionary<string, double> database)
+    {
+        Thread.Sleep(2000);
+
+        double balance = 0;
+        balance = database[name_customer];
+
+        if (balance > 0)
+            Console.WriteLine($"Cliente {name_customer} possui R${balance}: - Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+        else
+            Console.WriteLine($"Cliente não encontrado. - Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static void ImprimirTempoEmSegundos(Stopwatch stopwatch)
+    {
+        var tempoSegundos = stopwatch.ElapsedMilliseconds/1000;
+
+
+        Console.WriteLine($"Levou {tempoSegundos} segundos para a execução");
+    }
+
+    static string PegarNomeAleatorio(Dictionary<string, double> dicionario)
+    {
+        List<string> clientes = new List<string>();
+
+        foreach (var item in dicionario)
+        {
+            clientes.Add(item.Key);
+        }
+
+        string resultado = "";
+        Random rnd = new Random();
+        resultado = clientes[rnd.Next(clientes.Count)];
+
+        return resultado;
+    }
+
+
+
+}
---
+++ Ex02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        ExecutarTodasAsTarefas(2);
+
+    }
+
+    static void AcertarBaseDeDados()
+    {
+        Console.WriteLine($"Começando a acertando Base de dados. - Thread ID:{Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(2000);
+        Console.WriteLine($"Finalizando o acerto na base de dados. - Thread ID:{Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static void EnviarEmail()
+    {
+        Console.WriteLine($"Enviando Email. - Thread ID:{Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(4000);
+        Console.WriteLine($"Email enviado. - Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static void LimparArquivosTemporarios()
+    {
+        Console.WriteLine($"Iniciando Limpeza de Arquivos Temporarios. - Thread ID:{Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(3000);
+        Console.WriteLine($"Finalizando Limpeza de Arquivos Temporários - Thread ID:{Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static void ExecutarTodasAsTarefas(int qtddThreads)
+    {
+        ParallelOptions op = new();
+        op.MaxDegreeOfParallelism = qtddThreads;
+
+        Stopwatch sw = Stopwatch.StartNew();
+        sw.Start();
+
+        Parallel.Invoke(op, () => AcertarBaseDeDados(),
+                        () => EnviarEmail(),
+                        () => LimparArquivosTemporarios()
+                       );
+
+
+        sw.Stop();
+        Console.WriteLine($"O processo levou {sw.ElapsedMilliseconds} ms");
+
+    }
+
+}
